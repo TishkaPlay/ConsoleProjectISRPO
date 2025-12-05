fa58ba0124abc104be35083261922b10b0129b3f
TishkaaPlay <tikhon42otmyanin@gmail.com>
2025-12-06 01:29:27 +0300

    создал классы clear, show, move

--- GameBoard.cs
+++ GameBoard.cs
@@ -8,6 +8,67 @@ namespace ConsoleProject
 {
     internal class GameBoard
     {
+        //поле 3х3
+        private char[,] board;
 
+        public GameBoard()
+        {
+            board = new char[3, 3];
+            Clear();
+        }
+
+        //Очистка поля
+        public void Clear()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+        }
+
+        //само поле
+        public void Show()
+        {
+            Console.WriteLine("=== Крестики-Нолики ===");
+            //номера столбцов
+            Console.WriteLine("    1   2   3");
+            //верхняя граница
+            Console.WriteLine("  +-----------+");
+
+            for (int row = 0; row < 3; row++)
+            {
+                Console.WriteLine($"{row + 1} |");
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write($" {board[row, col]} |");
+                }
+                Console.WriteLine();
+
+                //разделить
+                if (row < 2)
+                    Console.WriteLine("  +-----------+");
+            }
+            //низ
+            Console.WriteLine("  +-----------+");
+        }
+
+        //ход
+        public bool Move(int row, int col, char symbol)
+        {
+            //проверка, корды ли в пределах поля
+            if (row < 0 || row > 2 || col < 0 || col > 2)
+                return false;
+
+            //проверка пустой клетки
+            if (board[row, col] != ' ')
+                return false;
+
+            //ставим символ
+            board[row, col] = symbol;
+            return true;
+        }
     }
 }
