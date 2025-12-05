7698eaa395574e33f168a26df0a3b4a9713d7b37
TishkaaPlay <tikhon42otmyanin@gmail.com>
2025-12-05 22:22:10 +0300

    class player - написан

--- Player.cs
+++ Player.cs
@@ -8,5 +8,13 @@ namespace ConsoleProject
 {
     internal class Player
     {
+        public string Name { get; set; }
+        public char Symbol { get; set; }
+
+        public Player(string name, char symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+        }
     }
 }
